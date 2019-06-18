f74dfd6cd300cdcf6579ceea0496b85e55ec5aac
Kavya <kavyamaniyan13@gmail.com>
2019-06-18 21:39:53 +0530

    modified base url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: `http://localhost:3005`,
-    // baseURL: `/`,
+    // baseURL: `http://localhost:3005`,
+    baseURL: `/`,
     
 })
 
