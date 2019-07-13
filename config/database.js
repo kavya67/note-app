f3a40e5f8e12d45113fce667b4aea00ea1ebb238
Kavya <kavyamaniyan13@gmail.com>
2019-07-13 20:36:07 +0530

    modified packages

--- config/database.js
+++ config/database.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose')
 //connectexpress to mongo via mongoose 
 //configure the promise library to be ES6 Promise
 const path = `mongodb://localhost:27017/blog-feb-app`
-const connection_uri = process.env.MONGODB_URI || `mongodb+srv://techmakers:Kavz@256@cluster0-n7fpr.mongodb.net/test?retryWrites=true&w=majority`
+
  
 
 mongoose.Promise = global.Promise
 
 
 //connect to db
-mongoose.connect(connection_uri, {useNewUrlParser:true})
+mongoose.connect(path, {useNewUrlParser:true})
 .then(()=>{
     console.log('connected to db')
 })
